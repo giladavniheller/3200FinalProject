3786c3cf59120c85a264fe5154200bf671a00229
phigarcia <phigarcia39@gmail.com>
2023-04-16 15:49:35 -0400

    added get route for getting users

--- flask-app/src/users/users.py
+++ flask-app/src/users/users.py
@@ -4,3 +4,25 @@ from src import db
 
 
 users = Blueprint('users', __name__)
+
+# get list of all users
+@users.route('/users', methods=['GET'])
+def get_users():
+    cursor = db.get_db().cursor()
+    cursor.execute('select user_id, first_name,\
+        last_name, email, city, state, zip from users')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# get list of past concerts a user has attended
+
+# get a list of concerts a user wants to attend
+
+# update a user's profile information 
