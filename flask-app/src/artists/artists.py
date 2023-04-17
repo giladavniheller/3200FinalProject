b1c2cd4cbbb3cb0ceb64ebedf70f617456e7d500
giladavniheller <gilavniheller@gmail.com>
2023-04-17 14:11:33 -0400

    more functionality baby

--- flask-app/src/artists/artists.py
+++ flask-app/src/artists/artists.py
@@ -4,3 +4,19 @@ from src import db
 
 
 artists = Blueprint('artists', __name__)
+
+# get list of all users
+@artists.route('/artists', methods=['GET'])
+def get_artists():
+    cursor = db.get_db().cursor()
+    cursor.execute('select artist_id, artist_name,\
+        first_name, last_name, email, monthly_listener_count from Artists')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
