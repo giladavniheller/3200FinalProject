814d3d7159db666bc2ef6caa43d2718cbfc2c968
giladavniheller <gilavniheller@gmail.com>
2023-04-17 14:37:18 -0400

    Finalizing venues routes

--- flask-app/src/venues/venues.py
+++ flask-app/src/venues/venues.py
@@ -173,3 +173,42 @@ def assign_openers(openers, concert_id):
         db.get_db().commit()
 
     return 'Success!'
+
+# get list of all concerts
+@venues.route('/concerts', methods=['GET'])
+def get_concerts():
+    cursor = db.get_db().cursor()
+    cursor.execute('''select * from Concerts c JOIN Venues as v on c.venue_id = v.venue_id
+                   JOIN PerformsBridge as pb on pb.concert_id = c.concert_id
+                   JOIN Artists as a on a.artist_id = pb.artist_id''')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
+# delete a concert listing
+@venues.route('/delete_concert', methods=['DELETE'])
+def delete_concert():
+    theData = request.json
+    current_app.logger.info(theData) 
+
+    concert_id_use = theData['concert_id']
+    #lName = theData['concert_id']
+    user_id_use = request.args.get('user_id')
+    #concert_id_use = request.args.get('concert_id')
+        
+    query = 'DELETE FROM Concerts WHERE concert_id = "'
+    query += str(concert_id_use) + '"'
+    current_app.logger.info(query)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Success!'
